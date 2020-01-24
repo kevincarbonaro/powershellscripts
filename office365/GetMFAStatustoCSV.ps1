57d943f68d0ebcac13a81fdbdd17d38a15be83dc
Kevin <kevin.carbonaro@gmail.com>
2020-01-24 14:54:09 +0100

    Get MFA Status initial upload

---
+++ office365/GetMFAStatustoCSV.ps1
@@ -0,0 +1,144 @@
+﻿# Get MFA Status Report to CSV
+# Credits for some bits and pieces of code: currently unknown
+
+Param
+(
+    [Parameter(Mandatory = $false)]
+    [switch]$DisabledOnly,
+    [switch]$EnabledOnly,
+    [switch]$EnforcedOnly,
+    [switch]$ConditionalAccessOnly,
+    [switch]$AdminOnly,
+    [switch]$LicensedUserOnly,
+    [Nullable[boolean]]$SignInAllowed = $null,
+    [string]$UserName,
+    [string]$Password
+)
+
+# Check for MSOnline module
+$Modules=Get-Module -Name MSOnline -ListAvailable
+if($Modules.count -eq 0)
+{
+  Install-Module MSOnline
+}
+
+
+# Output file path
+# Make sure you have a folder in c: called tmp
+# TODO: check if folder exists and prompt the user to create it automatically
+$ExportCSVReport="C:\tmp\MFAStatusReport.csv"
+
+#Storing credential in script for scheduling purpose/ Passing credential as parameter
+if(($UserName -ne "") -and ($Password -ne ""))
+{
+ $SecuredPassword = ConvertTo-SecureString -AsPlainText $Password -Force
+ $Credential  = New-Object System.Management.Automation.PSCredential $UserName,$SecuredPassword
+ Connect-MsolService -Credential $credential
+}
+else
+{
+ Connect-MsolService | Out-Null
+}
+$Result=""
+$Results=@()
+$UserCount=0
+$PrintedUser=0
+
+#Loop through each user
+Get-MsolUser -All | foreach{
+ $UserCount++
+ $DisplayName=$_.DisplayName
+ $Upn=$_.UserPrincipalName
+ $MFAStatus=$_.StrongAuthenticationRequirements.State
+ $MethodTypes=$_.StrongAuthenticationMethods
+ $RolesAssigned=""
+ Write-Progress -Activity "`n     Processed user count: $UserCount "`n"  Currently Processing: $DisplayName"
+ if($_.BlockCredential -eq "True")
+ {
+  $SignInStatus="False"
+  $SignInStat="Denied"
+ }
+ else
+ {
+  $SignInStatus="True"
+  $SignInStat="Allowed"
+ }
+
+ if($_.IsLicensed -eq $true)
+ {
+  $LicenseStat="Licensed"
+ }
+ else
+ {
+  $LicenseStat="Unlicensed"
+ }
+
+ #Check for user's Admin role
+ $Roles=(Get-MsolUserRole -UserPrincipalName $upn).Name
+ if($Roles.count -eq 0)
+ {
+  $RolesAssigned="No roles"
+  $IsAdmin="False"
+ }
+ else
+ {
+  $IsAdmin="True"
+  foreach($Role in $Roles)
+  {
+   $RolesAssigned=$RolesAssigned+$Role
+   if($Roles.indexof($role) -lt (($Roles.count)-1))
+   {
+    $RolesAssigned=$RolesAssigned+","
+   }
+  }
+ }
+
+ #Check for Disabled
+ if($MFAStatus -eq $null)
+ {
+  $MFAStatus='Disabled'
+ }
+
+  $Methods=""
+  $MethodTypes=""
+  $MethodTypes=$_.StrongAuthenticationMethods.MethodType
+  $DefaultMFAMethod=($_.StrongAuthenticationMethods | where{$_.IsDefault -eq "True"}).MethodType
+  $MFAPhone=$_.StrongAuthenticationUserDetails.PhoneNumber
+  $MFAEmail=$_.StrongAuthenticationUserDetails.Email
+
+  if($MFAPhone -eq $Null)
+  { $MFAPhone="-"}
+  if($MFAEmail -eq $Null)
+  { $MFAEmail="-"}
+
+  if($MethodTypes -ne $Null)
+  {
+   $ActivationStatus="Yes"
+   foreach($MethodType in $MethodTypes)
+   {
+    if($Methods -ne "")
+    {
+     $Methods=$Methods+","
+    }
+    $Methods=$Methods+$MethodType
+   }
+  }
+
+  else
+  {
+   $ActivationStatus="No"
+   $Methods="-"
+   $DefaultMFAMethod="-"
+   $MFAPhone="-"
+   $MFAEmail="-"
+  }
+
+  #Print to output file
+  $PrintedUser++
+  $Result=@{'DisplayName'=$DisplayName;'UserPrincipalName'=$upn;'MFAStatus'=$MFAStatus;'ActivationStatus'=$ActivationStatus;'DefaultMFAMethod'=$DefaultMFAMethod;'AllMFAMethods'=$Methods;'MFAPhone'=$MFAPhone;'MFAEmail'=$MFAEmail;'LicenseStatus'=$LicenseStat;'IsAdmin'=$IsAdmin;'AdminRoles'=$RolesAssigned;'SignInStatus'=$SigninStat}
+  $Results= New-Object PSObject -Property $Result
+  $Results | Select-Object DisplayName,UserPrincipalName,MFAStatus,ActivationStatus,DefaultMFAMethod,AllMFAMethods,MFAPhone,MFAEmail,LicenseStatus,IsAdmin,AdminRoles,SignInStatus | Export-Csv -Path $ExportCSVReport -Notype -Append
+ }
+
+#Clean up session
+Get-PSSession | Remove-PSSession
