7456d5137d44dde4c84bde520b745dce79a5c75e
Kevin <kevin.carbonaro@gmail.com>
2020-02-27 16:04:04 +0100

    Added correct variables, no logic yet

--- cw-automate/veeam-backup/veeam-backup.ps1
+++ cw-automate/veeam-backup/veeam-backup.ps1
@@ -5,7 +5,7 @@
 # Event ID	 Name	                        Description	                                                                                                                       Severity
 # 110        Backup job started	            <Job type> job '<Job name>' has been started.                                                                                      Info
 # 190	     Backup job finished	        The <Job type> Job '<Job name>' has finished with <State name> state.                                                              Info,Warning,Error
-# 24010	     License installed	            <license type> License key for Veeam Backup & Replication <edition> has been installed.                                           Info
+# 24010	     License installed	            <license type> License key for Veeam Backup & Replication <edition> has been installed.                                            Info
 # 24020	     License expiring	            <license type> License key for Veeam Backup & Replication <edition>  is about to expire in <number of days> of Days.               Warning
 # 24022	     License evaluation expiring	<licanse type> Evalution license key for Veeam Backup & Replication <edition>  is about to expire in <nuymber of days> of Days.    Warning
 # 24030	     License expired	            <license type> License key for Veeam Backup & Replication <edition> has expired.                                                   Error
@@ -21,7 +21,7 @@ $licenseExpiringId = 24020
 $licenseEvaluationExpiringId = 24022
 $licenseExpiredId = 24030
 $licenseSupportExpiringId = 24040
-$licenseSupportExpired = 24050
+$licenseSupportExpiredId = 24050
 
 # Event IDs human readable error messages
 $backupJobStartedMessage = "Backup Job has started."
@@ -37,7 +37,9 @@ $licenseSupportExpired = "Support contract license has expired."
 
 # Counters for last 7 Days
 $backupJobStartedCountWeek = 0
-$backupJobFinishedCountWeek = 0
+$backupJobFinishedInfoCountWeek = 0
+$backupJobFinishedWarningCountWeek = 0
+$backupJobFinishedErrorCountWeek = 0
 $licenseInstalledCountWeek = 0
 $licenseExpiringCountWeek = 0
 $licenseEvaluationExpiringCountWeek = 0
@@ -47,7 +49,9 @@ $licenseSupportExpired = 0
 
 # Counters for last 24 Hrs
 $backupJobStartedCountDay = 0
-$backupJobFinishedCountDay = 0
+$backupJobFinishedInfoCountDay = 0
+$backupJobFinishedWarningCountDay = 0
+$backupJobFinishedErrorCountDay = 0
 $licenseInstalledCountDay = 0
 $licenseExpiringCountDay = 0
 $licenseEvaluationExpiringCountDay = 0
@@ -60,57 +64,67 @@ $log = ""
 $yesterday = (Get-Date) - (New-TimeSpan -Day 1)
 $week = (Get-Date) - (New-TimeSpan -Day 6)
 
-# **************************************************************************
-# ***** TODO: Modify below code for Veeam backup Job Monitoring Logic ******
-# **************************************************************************
-
-#Get Log Counts for each event ids for week
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$week } | Where-Object { $_.Id -eq '11' }
-$backupErrorStopEventCountWeek = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$week } | Where-Object { $_.Id -eq '18' }
-$scheduleBackupFailedEventCountWeek = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$week } | Where-Object { $_.Id -eq '1' }
-$backupStartEventCountWeek = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$week } | Where-Object { $_.Id -eq '3' }
-$backupSuccessStopEventCountWeek = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$week } | Where-Object { $_.Id -eq '10' }
-$backupWarningStopEventCountWeek = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$week } | Where-Object { $_.Id -eq '14' }
-$cbpVersionUpgraeNtfEventCountWeek = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$week } | Where-Object { $_.Id -eq '16' }
-$cbpStorageQuotaExceededNtfEventCountWeek = $log.Count
+#Get Log Counts for each  ids for week
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$week } | Where-Object { $_.Id -eq '110' }
+$backupJobStartedCountWeek = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$week } | Where-Object { $_.Id -eq '190' }
+$backupJobFinishedInfoCountWeek = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$week } | Where-Object { $_.Id -eq '190' }
+$backupJobFinishedWarningCountWeek = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$week } | Where-Object { $_.Id -eq '190' }
+$backupJobFinishedErrorCountWeek = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$week } | Where-Object { $_.Id -eq '24010' }
+$licenseInstalledCountWeek = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$week } | Where-Object { $_.Id -eq '24020' }
+$licenseExpiringCountWeek = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$week } | Where-Object { $_.Id -eq '24022' }
+$licenseEvaluationExpiringCountWeek = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$week } | Where-Object { $_.Id -eq '24030' }
+$licenseExpiredCountWeek = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$week } | Where-Object { $_.Id -eq '24040' }
+$licenseSupportExpiringCountWeek = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$week } | Where-Object { $_.Id -eq '24050' }
+$licenseSupportExpiredCountWeek = $log.Count
 
 #Get Log Counts for each event ids for yesterday
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '11' }
-$backupErrorStopEventCountDay = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '18' }
-$scheduleBackupFailedEventCountDay = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '1' }
-$backupStartEventCountDay = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '3' }
-$backupSuccessStopEventCountDay = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '10' }
-$backupWarningStopEventCountDay = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '14' }
-$cbpVersionUpgraeNtfEventCountDay = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '16' }
-$cbpStorageQuotaExceededNtfEventCountDay = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '110' }
+$backupJobStartedCountDay = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '190' }
+$backupJobFinishedInfoCountDay = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '190' }
+$backupJobFinishedWarningCountDay = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '190' }
+$backupJobFinishedErrorCountDay = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '24010' }
+$licenseInstalledCountDay = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '24020' }
+$licenseExpiringCountDay = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '24022' }
+$licenseEvaluationExpiringCountDay = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '24030' }
+$licenseExpiredCountDay = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '24040' }
+$licenseSupportExpiringCountDay = $log.Count
+$log = Get-Win -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '24050' }
+$licenseSupportExpiredCountDay = $log.Count
 
+# Initialize object for response
+$obj = @{}
+
+# ******************** TO DO LOGIC *******************
 if ($backupSuccessStopEventCountDay -eq 0) {
     # No Backup was successful yet
     # Return a failure response
-    $obj = @{}
     $obj.lastBackupStatus = "Failed"
-    $obj.eventLogYesterday = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$yesterday } | Format-Table | Out-String
-    $Final = [string]::Join("|",($obj.GetEnumerator() | %{$_.Name + "=" + $_.Value}))
 } else {
     # Backup was successful
     # Return a success response
-    $obj = @{}
-    $obj.lastBackupStatus = "Success"
-    $obj.eventLogYesterday = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$yesterday } | Format-Table | Out-String
-    $Final = [string]::Join("|",($obj.GetEnumerator() | %{$_.Name + "=" + $_.Value}))
+    $obj.lastBackupStatus = "Success"    
 }
+
+# Prepare object for CW Automate
+$obj.eventLogYesterday = Get-WinEvent -FilterHashtable @{ LogName='Veeam Backup'; StartTime=$yesterday } | Format-Table | Out-String
+$Final = [string]::Join("|",($obj.GetEnumerator() | %{$_.Name + "=" + $_.Value}))
 
 Write-Output $Final
 
