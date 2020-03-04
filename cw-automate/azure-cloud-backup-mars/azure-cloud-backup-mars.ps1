219dc611665f419aabb635e379a526b8fd11c82b
Kevin <kevin.carbonaro@gmail.com>
2020-03-04 10:51:38 +0100

    Cleanup of unused code and added agent update status

--- cw-automate/azure-cloud-backup-mars/azure-cloud-backup-mars.ps1
+++ cw-automate/azure-cloud-backup-mars/azure-cloud-backup-mars.ps1
@@ -10,78 +10,20 @@
 #Warning	    CBP_VERSION_UPGRADE_NTF_EVENT	      14	     A newer version of Windows Azure Backup Agent is required
 #Warning	    CBP_STORAGE_QUOTA_EXCEEDED_NTF_EVENT  16	     Storage quota limit is approaching 80 percent
 
-# Event IDs for all type of errors
-$backupErrorStopId = 11
-$scheduleBackupFailedId = 18
-$backupStartId = 1
-$backupSuccessStopId = 3
-$backupWarningStopId = 10
-$cbpVersionUpgradeNtfId = 14
-$cbpStorageQuotaExceededNtfId = 16
-
-# Event IDs human readable error messages
-$backupErrorStopMessage = "The backup operation has completed with errors."
-$scheduleBackupFailedMessage = "Scheduled backup failed in initialization phase"
-$backupStartMessage = "The backup operation has started."
-$backupSuccessStopMessage = "The backup operation has completed."
-$backupWarningStopMessage = "The backup operation has completed with warnings."
-$cbpVersionUpgradeNtfMessage = "A newer version of Windows Azure Backup Agent is required"
-$cbpStorageQuotaExceededNtfMessage = "Storage quota limit is approaching 80 percent."
-
-# Counters for last 7 Days
-$backupErrorStopEventCountWeek = 0
-$scheduleBackupFailedEventCountWeek = 0
-$backupStartEventCountWeek = 0
-$backupSuccessStopEventCountWeek = 0
-$backupWarningStopEventCountWeek = 0
-$cbpVersionUpgradeNtfEventCountWeek = 0
-$cbpStorageQuotaExceededNtfEventCountWeek = 0
-
 # Counters for last 24 Hrs
-$backupErrorStopEventCountDay = 0
-$scheduleBackupFailedEventCountDay = 0
-$backupStartEventCountDay = 0
 $backupSuccessStopEventCountDay = 0
-$backupWarningStopEventCountDay = 0
 $cbpVersionUpgradeNtfEventCountDay = 0
-$cbpStorageQuotaExceededNtfEventCountDay = 0
 
 cls
 $log = ""
 $yesterday = (Get-Date) - (New-TimeSpan -Day 1)
-$week = (Get-Date) - (New-TimeSpan -Day 6)
-
-#Get Log Counts for each event ids for week
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$week } | Where-Object { $_.Id -eq '11' }
-$backupErrorStopEventCountWeek = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$week } | Where-Object { $_.Id -eq '18' }
-$scheduleBackupFailedEventCountWeek = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$week } | Where-Object { $_.Id -eq '1' }
-$backupStartEventCountWeek = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$week } | Where-Object { $_.Id -eq '3' }
-$backupSuccessStopEventCountWeek = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$week } | Where-Object { $_.Id -eq '10' }
-$backupWarningStopEventCountWeek = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$week } | Where-Object { $_.Id -eq '14' }
-$cbpVersionUpgradeNtfEventCountWeek = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$week } | Where-Object { $_.Id -eq '16' }
-$cbpStorageQuotaExceededNtfEventCountWeek = $log.Count
 
 #Get Log Counts for each event ids for yesterday
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '11' }
-$backupErrorStopEventCountDay = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '18' }
-$scheduleBackupFailedEventCountDay = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '1' }
-$backupStartEventCountDay = $log.Count
 $log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '3' }
 $backupSuccessStopEventCountDay = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '10' }
-$backupWarningStopEventCountDay = $log.Count
+
 $log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '14' }
 $cbpVersionUpgradeNtfEventCountDay = $log.Count
-$log = Get-WinEvent -FilterHashtable @{ LogName='CloudBackup'; StartTime=$yesterday } | Where-Object { $_.Id -eq '16' }
-$cbpStorageQuotaExceededNtfEventCountDay = $log.Count
 
 # Initialize object for response
 $obj = @{}
@@ -90,6 +32,8 @@ $obj = @{}
 if ($cbpVersionUpgradeNtfEventCountDay -gt 0) {
     # Return an update response
     $obj.updateStatus = "Update"
+} else {
+    $obj.updateStatus = ""
 }
 
 # Check if the last backup was successful or failed
